cf090ec9821a89ce0e2b4b4bbf501b1339df33be
marcelo <capp59marcelo@hotmail.com>
2019-03-11 19:03:57 -0300

    criado componentes

---
+++ src/Componentes/button/Button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./Button.css"
+const Button =({label}) =>(
+    <button className="button">{label}</button>
+);
+
+export default Button;
---
+++ src/Componentes/displayResult/DisplayResult.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./DisplayResult.css";
+
+const DisplayResult =({value}) =>(
+    <div className="displayResult">{value}</div>
+);
+
+export default DisplayResult;
